46603d63e9c8f726cdfa30f5c1d7ca74b0b4168e
Duncan <bonsairobo@gmail.com>
2020-11-14 11:25:49 -0800

    export DirtyChunks

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! Bevy plugins for the building-blocks voxel library.
+
 mod map;
 mod map_io;
 mod thread_local_resource;
@@ -8,7 +10,7 @@ pub use thread_local_resource::{ThreadLocalResource, ThreadLocalResourceHandle};
 pub use map::{default_chunk_map, VoxelMap, VoxelPalette};
 
 // Systems and resources that facilitate voxel access.
-pub use map_io::{ChunkCacheConfig, MapIoPlugin, ThreadLocalVoxelCache, VoxelEditor};
+pub use map_io::{ChunkCacheConfig, DirtyChunks, MapIoPlugin, ThreadLocalVoxelCache, VoxelEditor};
 
 /// You can use your own type of voxel, but it must implement this trait.
 pub trait Voxel: 'static + Copy + Default + Send + Sync {
